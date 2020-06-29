ce7e8ff95e2936648e9da4ee519b3408922848ae
cherishsince <cherishsince@163.com>
2020-06-29 14:29:52 +0800

    - 初始化：添加项目文件

---
+++ example-web/src/main/java/example/web/EmailController.java
@@ -0,0 +1,46 @@
+package example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Controller
+public class EmailController {
+    private static Pattern pattern;
+    private Matcher matcher;
+
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+
+    public EmailController() {
+        pattern = Pattern.compile(EMAIL_REGEX);
+    }
+
+    @GetMapping("/form")
+    public ModelAndView showIndex() {
+        return new ModelAndView("index");
+    }
+
+    @PostMapping("/validate")
+    public ModelAndView validateEmail(@RequestParam String email) {
+        ModelAndView modelAndView = new ModelAndView();
+        boolean isValid = validate(email);
+        if (!isValid) {
+            modelAndView.setViewName("index");
+            modelAndView.addObject("message", "Email is invalid");
+        } else {
+            modelAndView.setViewName("success");
+            modelAndView.addObject("email", email);
+        }
+        return modelAndView;
+    }
+
+    private boolean validate(String email) {
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
---
+++ example-web/src/main/webapp/views/success.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: HML
+  Date: 29/05/2020
+  Time: 11:26 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Success</title>
+</head>
+<body>
+<h3>Hi ${email}</h3>
+</body>
+</html>
